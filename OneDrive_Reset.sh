760ba24645e9dc26ab996c9a3255dde37f36e240
John Hirokawa <125434424+johnhirokawa@users.noreply.github.com>
2025-02-13 09:39:10 -1000

    Create OneDrive_Reset.sh

---
+++ OneDrive_Reset.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# OneDrive - Reset
+# Description: This script resets the OneDrive application by running the reset command and reopening the app.
+
+# Reset OneDrive
+/Applications/OneDrive.app/Contents/Resources/ResetOneDriveApp.command
+
+# Reopen OneDrive
+open /Applications/OneDrive.app
+
+# Code Credit: John Hirokawa
